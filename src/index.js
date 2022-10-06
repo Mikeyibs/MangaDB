299459e079849b1cbade6f0edeed22d2300192bc
Mikeyibs <myboh7@gmail.com>
2022-10-05 20:34:24 -0400

    working on api still

--- src/index.js
+++ src/index.js
@@ -1,65 +1,14 @@
 const searchInput = document.querySelector("[data-search]")
 
-var query = `
-query ($id: Int, $page: Int, $perPage: Int, $search: String) {
-  Page (page: $page, perPage: $perPage) {
-    pageInfo {
-      total
-      currentPage
-      lastPage
-      hasNextPage
-      perPage
-    }
-    media (id: $id, search: $search) {
-      id
-      title {
-        romaji
-      }
-    }
-  }
-}
-`;
-
-
-var variables = {
-    search: "Fate/Zero",
-    page: 1,
-    perPage: 3
-};
-
-var url = 'https://graphql.anilist.co',
-    options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-            query: query,
-            variables: variables
-        })
-    };
-
 searchInput.addEventListener("input", e => {
     const value = e.target.value
     console.log(value)
 })
 
-fetch(url, options).then(handleResponse)
-                   .then(handleData)
-                   .catch(handleError);
+getManga(searchInput);
 
-function handleResponse(response) {
-    return response.json().then(function (json) {
-        return response.ok ? json : Promise.reject(json);
-    });
-}
-                
-function handleData(data) {
-    console.log(data);
-}
-                
-function handleError(error) {
-    alert('Error, check console');
-    console.error(error);
+async function getManga(searchInput) {
+  const temp = await fetch(
+    'https://api.jikan.moe/v4/manga/${searchInput}/full'
+    )
 }
