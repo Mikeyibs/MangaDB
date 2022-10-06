8b74f23a7a3ebe0e3347afce54cb15f1cc361165
Mikeyibs <myboh7@gmail.com>
2022-10-05 23:07:51 -0400

    Figured out how to get access to a specific manga with the title

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-const searchInput = document.querySelector("[data-search]")
+const title = document.querySelector("[data-search]");
+const jikanAPI = 'https://api.jikan.moe/v4/manga';
+const HEADERS = {
+  'Content-Type': 'application/json'
+};
 
-searchInput.addEventListener("input", e => {
+title.addEventListener("input", e => {
     const value = e.target.value
     console.log(value)
-})
+});
 
-getManga(searchInput);
-
-async function getManga(searchInput) {
-  const temp = await fetch(
-    'https://api.jikan.moe/v4/manga/${searchInput}/full'
-    )
+const manga = {
+  title
 }
+
+const query = "Berserk"
+
+fetch(`https://api.jikan.moe/v4/manga?q=${query}`).then(response => {
+  if(!response.ok) {
+    throw new Error('Request failed')
+  }
+  return response.json()
+}).then(data => {
+  console.log(data)
+}).catch(error => console.log(error));
+
+
